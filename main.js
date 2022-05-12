606d39069e44f561203afe19a9b8b38d7d5906f8
krypton <kelxwii@gmail.com>
2022-05-12 15:21:19 -0400

    fixed an error with the service worker

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ addEventListener("load", () => {
 })
 
 function subscribe() {
-    let sw = navigator.serviceWorker.ready("./sw.js");
+    let sw = navigator.serviceWorker.ready;
     let push = sw.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: "BN6bSsnsvYOD35xp2oPVUaBQxJT4CCC0CYspiZdg7GIxNzxURRzzaUuhkBVgWRBidzsug1_5mU_mTvgXl_e-mXI"
